6aa8d9a45ab3173838e67ac20704d34be689f6e8
Aditya Mehar <aditya.mehar@zehntech.com>
2024-02-14 18:46:26 +0530

    Edit and Destroy docs -Destroy

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -24,20 +24,26 @@ class DocsController < ApplicationController
   end
 
   def edit
-
+    @doc = Doc.find(params[:id])
   end
 
   def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    @doc.destroy
+    redirect_to docs_path
   end
 
   private
 
     def find_doc
-
+      @doc = Doc.find(params[:id])
     end
 
     def doc_params
